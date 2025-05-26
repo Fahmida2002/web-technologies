e296b3c033071dd1ce7b0ae0b0b267dfb53231b1
Fahmida Yasmin <139944264+Fahmida2002@users.noreply.github.com>
2025-05-26 21:25:50 +0600

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); 
+session_unset();
+// Destroy the session
+session_destroy();
+// Inform the user
+echo "You are now redirected";
+header("refresh:2; url=index.html");
+exit;
+?>
